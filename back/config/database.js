0d0336952f233f8221fad793b288ce3d964fd353
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2023-08-25 23:50:56 -0500

    Conexion a la base de datos
    Conexion a la base de datos  y carpeta de models

---
+++ back/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+//crear un metodo para generar la conexion a la base de datos
+const connectDatabase = ()=>{
+    mongoose.connect(process.env.DB_LOCAL_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(con => {
+        console.log(`Base de datos mongo conectada con el servidor: ${con.connection.host}`)
+    }).catch(con => {
+        console.log(`No se logro la conexion con la base de datos`)
+    })
+   
+}
+
+module.exports = connectDatabase;
