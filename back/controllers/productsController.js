7db48fa2b054746c29f5ae744e3f5c596f71a48e
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2023-09-01 02:25:53 -0500

    CRUD PRODUCTOS
    CRUD PRODUCTOS

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,20 +1,76 @@
 const producto = require("../models/productos")
 
 //Ver la lista de productos
-exports.getProducts = (req, res, next) => {
+exports.getProducts = async (req, res, next) => {
+  const productos = await producto.find();
+  res.status(200).json({
+    success: true,
+    cantidad: productos.length,
+    productos,
+  })
+}
+
+//Ver un producto por ID
+exports.getProductById = async (req, res, next) => {
+  const product = await producto.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "No Encontramos Este Producto",
+    })
+  }
+  res.status(200).json({
+    success: true,
+    message: "Aqui debajo encuentras información de tu producto:",
+    product,
+  })
+}
+
+//Update un producto
+exports.updateProduct = async (req, res, next) => {
+  let product = await producto.findById(req.params.id); //Variable de tipo modificable
+  if (!product) {
+    // verifico que el producto no exista para finalizar el proceso
+    return res.status(404).json({
+      success: false,
+      message: "No encontramos este producto",
+    })
+  }
+  // si el objeto existia, entonces si ejecuto la actualizacion
+   product = await producto.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, //Valido solo los atributos nuevos o actualizados
+    runValidators: true,
+  })
+  //Respondo Ok si el producto si se actualizó
+  res.status(200).json({
+    success: true,
+    message: "Producto actualizado correctamente",
+    product,
+  })
+}
+
+
+//Eliminar un producto
+exports.deleteProduct= async (req, res, next)=>{
+    const product = await producto.findById(req.params.id) //Variable de tipo modificable
+    if (!product) { // verifico que el producto no exista para finalizar el proceso
+          return res.status(404).json({
+          success: false,
+          message: "No encontramos este producto",
+        })
+    }
+    await product.deleteOne();//Eliminamos el proceso
     res.status(200).json({
-        success: true,
-        message: "En esta ruta ud va a poder ver todos los productos"
+        success:true,
+        message:"Producto eliminado correctamente",
     })
 }
-
 //Crear nuevo producto /api/productos
-exports.newProduct =async (req, res, next) => {
-    const product=await producto.create (req.body);
+exports.newProduct = async (req, res, next) => {
+  const product = await producto.create(req.body);
 
-    res.status(201).json ({
-        success:true,
-        product
-    })   
+  res.status(201).json({
+    success: true,
+    product,
+  })
 }
-
