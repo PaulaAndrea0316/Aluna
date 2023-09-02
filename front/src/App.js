cc78b13166b6aa38ac34fe4db2b5ad3cdc2dbe77
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2023-09-02 02:03:57 -0500

    INSTALACION REACT Y DEPENDENCIAS
    Se instala react y las dependencias, se crea carpeta front y se incia con el modelado de la pagina

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda Aluna</h1>
+      <img src = "./images/Aluna.png" alt = "Aqui va el logo"></img>
+  
+    </div>
+  );
+}
+
+export default App;
