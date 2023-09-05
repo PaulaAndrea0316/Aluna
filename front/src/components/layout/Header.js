2e399e0508c3e72d4541174250901a0c81469230
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2023-09-05 15:43:31 -0500

    cambios instlacion

--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -1,38 +1,46 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 const Header = () => {
   return (
     <Fragment>
-      <nav className='navbar row'>
-        <div className='col-12 col-md-3'>
-            <div className='navbar-brand'>
-                <img src="./images/Aluna.png" alt="Aluna Store Logo"></img>
-            </div>
+      <nav className="navbar row">
+        <div className="col-12 col-md-3">
+          <div className="navbar-brand">
+            <img src="./images/Aluna.png" alt="Aluna Store Logo"></img>
+          </div>
         </div>
 
-        <div className='col-12 col-md-6 mt-2 mt-md-0'>
-            <div className = "input-group">
-                <input
-                    type= "text"
-                    id="search_field"
-                    class= "form-control" 
-                    placeholder='Que producto busca?'></input>
-                    <div class = "input-group-append">
-                        <button id= "search-btn" class = "btn">
-                        <i class = "fa fa-search" aria-hidden = "true">Buscar</i>    
-                            
-                        </button>
-                    </div>
+        <div className="col-12 col-md-6 mt-2 mt-md-0">
+          <div className="input-group">
+            <input
+              type="text"
+              id="search_field"
+              class="form-control"
+              placeholder="Que producto busca?"
+            ></input>
+            <div class="input-group-append">
+              <button id="search-btn" class="btn">
+                <i class="fa fa-search" aria-hidden="true">
+                  Buscar
+                </i>
+              </button>
             </div>
+          </div>
         </div>
         <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-            <button className="btn" id ="login-btn">Inicie Sesión</button>
-            <span id = "cart" className="ml-3">Carrito</span>
-            <span className="ml-1" id = "cart_count">2</span>
+          <button className="btn" id="login-btn">
+            Inicie Sesión
+          </button>
+          <span id="cart" className="ml-3">
+            Carrito
+          </span>
+          <span className="ml-1" id="cart_count">
+            2
+          </span>
         </div>
-    </nav>
+      </nav>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
