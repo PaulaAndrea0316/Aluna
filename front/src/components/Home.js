dce417d47d5901ff8f721a3ef9e74bd6c72c4eda
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2023-09-16 02:18:22 -0500

    IMAGENES-HOME-INDEX
    Se crea la carpeta de productos, se reportan recursos en el home de link y alertas

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,116 +1,60 @@
 import React, { Fragment, useEffect } from "react";
 import MetaData from "./layout/MetaData";
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import { getProducts } from "../actions/productActions";
+import {Link} from 'react-router-dom'
+import { useAlert } from 'react-alert'
 
 const Home = () => {
+  const {loading,productos,error} = useSelector (state => state.products)
+  const alert = useAlert();
+
   const dispatch = useDispatch();
   useEffect(()=>{
-    dispatch(getProducts());    
+    if (error) {
+      return alert.error(error)
+    }
+
+    dispatch(getProducts()); 
+    alert.success("OK")   
   }, [dispatch])
 
 
   return (
     <Fragment>
-      <MetaData title = "Hecho a Mano"></MetaData>
+      {loading ? <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>:(
+        <Fragment>
+          <MetaData title = "Hecho a Mano"></MetaData>
       <h1 id="encabezado_productos">Ultimos Productos</h1>
 
       <section id="productos" className="container mt-5">
         <div className="row">
-            {/*producto1*/}
-          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+          {productos && productos.map(producto => (
+            <div  key={producto._id} className="col-sm-12 col-md-6 col-lg-3 my-3">
             <div className="card p-3 rounded">
               <img
                 className="card-img-top mx-auto"
-                src="./images/arboldelavidaazul.jpeg"
-                alt="Arbol de la vida"
-              ></img>
+                src={producto.imagen[0].url}
+                alt={producto.imagen[0].public_id}></img>
               <div className="card-body d-flex flex-column">
                 <h5 id="titulo_producto">
-                  <a href="http://localhost:3000"></a>Arbol De La Vida
-                </h5>
+                  <Link to={`/producto/${producto._id}`}>{producto.nombre}</Link></h5>
                 <div className="rating mt-auto">
                   <div className="rating-outer"></div>
-                  <div className="rating-inner"></div>
+                  <div className="rating-inner" style={{ width: `${(producto.calificacion/5)* 100}%`}}></div>
                 </div>
-                <span id="No_de_opiniones"> 5 Reviews</span>
+                <span id="No_de_opiniones">{producto.numCalificaciones} Reviews</span>
               </div>
-              <p className='cart-text'>$120.000</p><a href = 'http://localhost:3000' id = "view_btn" className='btn btn-block' >
+              <p className='cart-text'>${producto.precio}</p><Link to= {`/producto/${producto._id}`}  id = "view_btn" className='btn btn-block' >
                 Ver detalle 
-              </a>
+              </Link>
             </div>
           </div>
-          {/*producto2*/}
-          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-            <div className="card p-3 rounded">
-              <img
-                className="card-img-top mx-auto"
-                src="./images/Japa_Mala_de_sandalo.jpeg"
-                alt="Japa Mala de sandalo"
-              ></img>
-              <div className="card-body d-flex flex-column">
-                <h5 id="titulo_producto">
-                  <a href="http://localhost:3000"></a>Japa Mala De Sandalo
-                </h5>
-                <div className="rating mt-auto">
-                  <div className="rating-outer"></div>
-                  <div className="rating-inner"></div>
-                </div>
-                <span id="No_de_opiniones"> 4 Reviews</span>
-              </div>
-              <p className='cart-text'>$70.000</p><a href = 'http://localhost:3000' id = "view_btn" className='btn btn-block' >
-                Ver detalle 
-              </a>
-            </div>
-          </div>
-           {/*producto3*/}
-           <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-            <div className="card p-3 rounded">
-              <img
-                className="card-img-top mx-auto"
-                src="./images/Japa_Mala_de_Semillas.jpeg"
-                alt="Japa Mala de semillas"
-              ></img>
-              <div className="card-body d-flex flex-column">
-                <h5 id="titulo_producto">
-                  <a href="http://localhost:3000"></a>Japa Mala De Semillas
-                </h5>
-                <div className="rating mt-auto">
-                  <div className="rating-outer"></div>
-                  <div className="rating-inner"></div>
-                </div>
-                <span id="No_de_opiniones"> 5 Reviews</span>
-              </div>
-              <p className='cart-text'>$40.000</p><a href = 'http://localhost:3000' id = "view_btn" className='btn btn-block' >
-                Ver detalle 
-              </a>
-            </div>
-          </div>
-           {/*producto4*/}
-           <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-            <div className="card p-3 rounded">
-              <img
-                className="card-img-top mx-auto"
-                src="./images/Japa_Mala_de_Tulasi.jpeg"
-                alt="Japa Mala de Tulasi"
-              ></img>
-              <div className="card-body d-flex flex-column">
-                <h5 id="titulo_producto">
-                  <a href="http://localhost:3000"></a>Japa Mala De Tulasi
-                </h5>
-                <div className="rating mt-auto">
-                  <div className="rating-outer"></div>
-                  <div className="rating-inner"></div>
-                </div>
-                <span id="No_de_opiniones"> 8 Reviews</span>
-              </div>
-              <p className='cart-text'>$60.000</p><a href = 'http://localhost:3000' id = "view_btn" className='btn btn-block' >
-                Ver detalle 
-              </a>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
+        </Fragment>
+      )}    
     </Fragment>
   );
 };
