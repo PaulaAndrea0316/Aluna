487a3cd5d264159f482924440b3e2d4647aeb6b0
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2023-10-09 23:30:26 -0500

    rutas protegidas

--- back/routes/products.js
+++ back/routes/products.js
@@ -5,9 +5,9 @@ const {getProducts,newProduct,getProductById,updateProduct,deleteProduct}= requi
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 //Probemos Autenticacion
-router.route('/productos').get ( isAuthenticatedUser, authorizeRoles("admin"), getProducts) //Establecemos desde que ruta queremos ver el getProducts
-router.route('/producto/nuevo').post(newProduct) //establecemos la ruta
+router.route('/productos').get (getProducts) //Establecemos desde que ruta queremos ver el getProducts
+router.route('/producto/nuevo').post(isAuthenticatedUser, authorizeRoles("admin"),newProduct) //establecemos la ruta
 router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
-router.route('/producto/:id').put(updateProduct);//Creacion de la ruta de actualizacion
-router.route('/producto/:id').delete(deleteProduct); //Creacion de la ruta de eliminacion por id
+router.route('/producto/:id').put(isAuthenticatedUser, authorizeRoles("admin"),updateProduct);//Creacion de la ruta de actualizacion
+router.route('/producto/:id').delete(isAuthenticatedUser, authorizeRoles("admin"),deleteProduct); //Creacion de la ruta de eliminacion por id
 module.exports=router;
