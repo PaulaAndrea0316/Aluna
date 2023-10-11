3fe4fbf863c308fbecf6c91b0f124a491a442ff5
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2023-10-11 01:43:51 -0500

    LOGOUT-FORGOTPASSWORD
    LOGOUT-FORGOTPASSWORD

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,8 +1,10 @@
 const express=require("express");
-const { registroUsuario, loginUser } = require("../controllers/authController");
+const { registroUsuario, loginUser, logOut, forgotPassword } = require("../controllers/authController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 const router= express.Router();
 
 router.route('/usuario/registro').post(registroUsuario)
 router.route('/login').get(loginUser)
-
+router.route('/logout').get( isAuthenticatedUser, logOut)
+router.route("/forgotPassword").post(forgotPassword)
 module.exports= router
